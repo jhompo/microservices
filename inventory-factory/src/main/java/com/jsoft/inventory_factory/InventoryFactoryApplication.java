8cd1b2a36e58eb105ff2051713bfe888d59eb1b6
Jhonatan <jhompo@gmail.com>
2024-05-09 18:02:14 -0500

    register cliente in eureka

--- inventory-factory/src/main/java/com/jsoft/inventory_factory/InventoryFactoryApplication.java
+++ inventory-factory/src/main/java/com/jsoft/inventory_factory/InventoryFactoryApplication.java
@@ -2,8 +2,10 @@ package com.jsoft.inventory_factory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryFactoryApplication {
 
 	public static void main(String[] args) {
--- products-factory/src/main/java/com/jsoft/productsfactory/ProductsFactoryApplication.java
+++ products-factory/src/main/java/com/jsoft/productsfactory/ProductsFactoryApplication.java
@@ -3,10 +3,12 @@ package com.jsoft.productsfactory;
 //import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 //import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductsFactoryApplication {
 
 	public static void main(String[] args) {
